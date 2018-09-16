a6c0a4132db61439afaba90d16396701fc25a18b
Craig Oda <craig@oppkey.com>
2018-09-16 09:31:34 -0700

    created get cities function

---
+++ temperature-by-month/js/city2.js
@@ -0,0 +1,28 @@
+var cities = [
+    "honolulu", "palo alto", "los angeles",
+    "chicago", "boston"];
+
+d3.csv("city-month.csv").then(function(dataInFunction){
+    var cities = getCities(dataInFunction);
+    console.log(cities);
+});
+
+function getCities(data) {
+    // only use the first object
+    var firstLine = data[0];
+
+    // create an empty array to hold the names of cities
+    var arrayOfCities = [];
+
+    // loop through object
+    for (var key in firstLine) {
+        arrayOfCities.push(key);
+    }
+
+    // delete first element as the first
+    // element is the month
+
+    arrayOfCities.shift();
+
+    return (arrayOfCities);
+}
